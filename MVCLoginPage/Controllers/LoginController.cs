a310103d592aac8b3dfe3fd3edd60e6c3d2a1cad
Hüseyin Biçen <huseynbicen@outlook.com>
2019-03-18 02:18:16 +0300

    master

---
+++ MVCLoginPage/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using MVCLoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLoginPage.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(AdminLogin adminLogin)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                var q1 = db.AdminLogins.Where(x => x.eMail == adminLogin.eMail && x.Password == adminLogin.Password).FirstOrDefault();
+                if (q1 == null)
+                {
+                    adminLogin.LoginErrorMessage = "Yanlış E-Posta veya Şifre, Lütfen tekrar deneyiniz.";
+                    return View("Index",adminLogin);
+                }
+                else
+                {
+                    Session["UserID"] = adminLogin.Id;
+                    return RedirectToAction("Index","Employee");
+                }
+                
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+    }
+}
---
+++ MVCLoginPage/Controllers/PaymentController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCLoginPage.Models;
+
+namespace MVCLoginPage.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private EmployeeDBEntities db = new EmployeeDBEntities();
+
+        // GET: Payment
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Employee);
+            return View(payments.ToList());
+        }
+
+        // GET: Payment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payment/Create
+        public ActionResult Create()
+        {
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name");
+            return View();
+        }
+
+        // POST: Payment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Employee_Id,Date,Total")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // GET: Payment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // POST: Payment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Employee_Id,Date,Total")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // GET: Payment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payment payment = db.Payments.Find(id);
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
